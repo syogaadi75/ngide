2329c0c366479e5cdacb8399f8ad875da34b798d
YOA\yoaas <syogaadi75@gmail.com>
2024-08-26 19:08:00 +0700

    add cors

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const chromium = require('@sparticuz/chromium-min')
 const puppeteer = require('puppeteer-core')
 const path = require('path')
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 4000
+
+app.use(cors())
 
 async function getBrowser() {
   return puppeteer.launch({
